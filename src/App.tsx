f1d33627d811b2448627cf868689b0aee930c0a0
m0nty1 <satyricon1349@yahoo.com>
2025-12-17 23:33:42 +0200

    pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
-// E7na han-comment el ba2y mo2aqatan 3shan nerfa3 bsor3a mn gher errors
-// import Shop from './pages/Shop'; 
-// import AdminDashboard from './pages/AdminDashboard';
+import Shop from './pages/Shop';
+import ProductDetail from './pages/ProductDetail';
+import Login from './pages/Login';
+import AdminDashboard from './pages/AdminDashboard';
+import { AuthProvider } from './context/AuthContext';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          {/* Hannefta7 dol ba3dein lamma nen2el ba2et el files */}
-          <Route path="shop" element={<div className="p-10 text-center">Shop Page Coming Soon...</div>} />
-          <Route path="admin" element={<div className="p-10 text-center">Admin Panel Coming Soon...</div>} />
-        </Route>
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="shop" element={<Shop />} />
+            <Route path="product/:id" element={<ProductDetail />} />
+            <Route path="login" element={<Login />} />
+            <Route path="admin" element={<AdminDashboard />} />
+          </Route>
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
